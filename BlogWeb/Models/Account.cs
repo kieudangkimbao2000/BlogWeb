b9d85b2123113d2e55072e62a0b6e682dcf3bf21
Kiều Đặng Kim Bảo <kieudangkimbao2000@gmail.com>
2023-03-03 07:15:06 +0700

    Khoi tao Model, Controller, View cho caca thuc the

---
+++ BlogWeb/Models/Account.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogWeb.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(20)]
+        [MinLength(8)]
+        public string TenTaiKhoan { get; set; }
+        [Required]
+        [MaxLength(20)]
+        [MinLength(8)]
+        public string MatKhau { get; set; }
+        public byte[]? AnhDaiDien { get; set; }
+        [MaxLength(50)]
+        public string? HoTen { get; set; }
+        public bool GioiTinh { get; set; } = true;
+        [DataType(DataType.Date)]
+        public DateTime? NgaySinh { get; set; }
+        [MaxLength(12)]
+        [MinLength(10)]
+        public string? SDT { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(50)]
+        public string? Email { get; set; }
+        public bool Khoa { get; set; } = false;
+        public string LyDoKhoa { get; set; }
+        public bool Admin { get; set; } = false;
+
+        public Comment Comment { get; set; }
+    }
+}
